baa5df8a0e15465ae8f60537116405c96a320f29
Davide Sirico <davide.sirico@gmail.com>
2024-08-22 13:47:04 +0200

    .

--- commands.py
+++ commands.py
@@ -13,7 +13,6 @@ with open('file-commands.3', 'w') as sys.stdout:
     print("Connection status: ")
     print(connection.status())
 
-"""
     # mode 01
     print("mode 01")
     for i in range(0,95):
@@ -26,41 +25,6 @@ with open('file-commands.3', 'w') as sys.stdout:
     print("mode 02")
     for i in range(0,95):
         current_command = obd.commands[2][i]
-        res = connection.query(current_command, force=True)
-        print(i)
-        print(res.value)
-
-    # mode 03
-    print("mode 03")
-    current_command = obd.commands[3][0]
-    res = connection.query(current_command, force=True)
-    print(res.value)
-    time.sleep(1)
-
-    # mode 04
-    print("mode 04")
-    current_command = obd.commands[4][0]
-    res = connection.query(current_command, force=True)
-    print(res.value)
-    time.sleep(1)
-
-    # mode 06
-    print("mode 06")
-    for i in range(0,28):
-        current_command = obd.commands[6][i]
-        res = connection.query(current_command, force=True)
-        print(i)
-        print(res.value)
-"""
-    print("mode 07")
-    current_command = obd.commands[7][0]
-    res = connection.query(current_command, force=True)
-    print(res.value)
-    time.sleep(1)
-    # mode 09
-    print("mode 09")
-    for i in range(0,12):
-        current_command = obd.commands[9][i]
         res = connection.query(current_command, force=True)
         print(i)
         print(res.value)
