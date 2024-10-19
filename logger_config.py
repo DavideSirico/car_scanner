ea5b95e10308ff207715800b1f6b99ed48bfb904
Davide Sirico <davide.sirico@gmail.com>
2024-10-19 13:29:14 +0200

    .

--- logger_config.py
+++ logger_config.py
@@ -6,25 +6,21 @@ def setup_logger():
     logger = logging.getLogger('car_scanner')  # Create a logger for the entire app
     logger.setLevel(logging.DEBUG)  # Set the logging level
 
-    # Create a file handler
-    file_handler = logging.FileHandler('debug_log.log')
-    file_handler.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        # Create a file handler
+        file_handler = logging.FileHandler('debug_log.log')
+        file_handler.setLevel(logging.DEBUG)
 
-    # Create a stream handler (for console)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+        # Create a stream handler (for console)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
 
-    # Define a common format for log messages
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+        # Define a common format for log messages
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
 
-    # Add handlers to the logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-
-    # Add handlers to the logger
-    if not logger.hasHandlers():  # Avoid adding multiple handlers if already added
+        # Add handlers to the logger
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
     
